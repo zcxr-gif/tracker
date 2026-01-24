81cb89ee3bc8f0a7d6a756f15cd20e24b32f173a
zcxr-gif <hrantony2230@gmail.com>
2026-01-24 00:21:28 -0500

    mobile 3

--- flight.js
+++ flight.js
@@ -6,6 +6,7 @@ import { GroupFlightManager } from './groupFlightManager.js';
 import { updateActiveSectors } from './atcHighlights.js';
 import { NatTracksLayer } from './natTracksLayer.js';
 import { FlownPath3D } from './flownPath3D.js';
+import { MobileSettingsUI } from './MobileLandingUI.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
 
